8bc53760db1ec01c00a1f928d97c464c20c39c94
cadarius45 <cabrown676@gmail.com>
2024-06-28 02:54:59 -0500

    done with css for now

--- quizzez/forms.py
+++ quizzez/forms.py
@@ -2,10 +2,13 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
 
-class UserUpdateForm(UserChangeForm):
-    email = forms.EmailField()
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=False)
+    first_name = forms.EmailField(required=False)
+    last_name = forms.EmailField(required=False)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+  
         
